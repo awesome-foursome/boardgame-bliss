8e58bb182c54befbb8763ab179eded7e5db1f388
Simon Brown <simonandon@live.com.au>
2024-03-27 10:12:18 +0800

    Got getBestMove() to console log data from existing games

--- assets/js/chess-page.js
+++ assets/js/chess-page.js
@@ -1,10 +1,41 @@
 const chessForm = $('#chess-form');
 const submitBtn = $('#submit-btn');
+const usernameInput = $('#username-input');
+const depthInput = $('#depth-input');
 
 // function for form submit
 const getBestMove = function (event) {
 
+	// prevent default form behaviour
+	event.preventDefault();
+
+	// pull username and depth from form
+	const username = usernameInput.val();
+	const depth = depthInput.val();
+
+	// debug log
+	console.log('username:', username);
+	console.log('depth:', depth);
+
+	// set up request URL with username
+	const requestGameStateUrl = `https://api.chess.com/pub/player/${username}/games`
+
+	// debug log
+	console.log('requestGameStateUrl:', requestGameStateUrl);
+
+	// async fetch function
+	const fetchBestMove = async function () {
+		const response = await fetch(requestGameStateUrl);
+		const data = await response.json();
+
+		return data
+	};
+
+	// execute async fetch function
+	fetchBestMove()
+		.then(data => console.log(data));
+
 };
 
 // event handler for submit button
-chessForm.on('submit');
+submitBtn.on('click', getBestMove);
