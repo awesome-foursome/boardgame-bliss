b8def23e27c03702db385d67d3a3a86558c2742f
Simon Brown <simonandon@live.com.au>
2024-03-27 09:38:12 +0800

    Created event handler and empty function for chess-form

---
+++ assets/js/chess-page.js
@@ -0,0 +1,10 @@
+const chessForm = $('#chess-form');
+const submitBtn = $('#submit-btn');
+
+// function for form submit
+const getBestMove = function (event) {
+
+};
+
+// event handler for submit button
+chessForm.on('submit');
