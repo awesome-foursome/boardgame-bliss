158083ad97d89c238b2576fa1ac71c8f36ec78e3
@r0f33 <jiske.nicolson@gmail.com>
2024-04-03 13:54:36 +1030

    converted alot to bulma and made functions on js for image swaps

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -1,1 +1,31 @@
+const pages = document.querySelectorAll('#left div');
+const pageImages = document.querySelectorAll('#right > img');
 
+pages.forEach((page) => {
+    page.addEventListener('mouseover', () => {
+
+        const target = page.dataset.target;
+
+        pageImages.forEach(image => {
+            if (image.getAttribute('id') === target) {
+                image.classList.remove('is-hidden');
+            } else {
+                image.classList.add('is-hidden')
+            }
+        })
+    })
+});
+
+pages.forEach((page) => {
+    page.addEventListener('mouseout', () => {
+
+        const target = page.dataset.target;
+
+        pageImages.forEach(image => {
+            if (image.getAttribute('id') === target) {
+                image.classList.add('is-hidden');
+                document.querySelector('#index-img').classList.remove('is-hidden')
+            }
+        })
+    })
+});
