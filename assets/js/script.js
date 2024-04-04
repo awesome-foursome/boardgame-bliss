cab41db432c6ad44a65c9e0c023b8ca2743353de
@r0f33 <jiske.nicolson@gmail.com>
2024-04-04 22:42:09 +1030

    worked on navbar and updating theme function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,52 @@
-const themeSelector = document.getElementById('theme');
+// const themeSelector = document.getElementById('theme');
+// // const themeSelector = document.querySelectorAll('option');
+// const html = document.querySelector('html');
+
+// // toggle theme
+
+// // implement theme depending on saved preferences in local storage
+// function savedTheme() {
+//     const localStorageTheme = localStorage.getItem('theme');
+//     console.log(localStorageTheme);
+//     if (localStorageTheme !== null) {
+//         html.dataset.theme = localStorageTheme;
+//         themeSelector.value = localStorageTheme;
+//     }
+// };
+
+// // potentially put this into an on ready function at a later date
+// savedTheme();
+
+// // mobile burger menu
+// const burgerIcon = document.querySelector('#burger');
+// const navbarMenu = document.querySelector('#nav-links');
+
+// burgerIcon.addEventListener('click', () => {
+//     navbarMenu.classList.toggle('is-active');
+// });
+
+// _________________________________________________________________________________________________________________
+const themeSelector = document.querySelectorAll('option');
 const html = document.querySelector('html');
 
-// toggle theme
-themeSelector.addEventListener('change', () => {
-    const toggleState = themeSelector.value;
+// // toggle theme
+themeSelector.forEach((theme) => {
+    theme.addEventListener('click', () => {
+        const toggleState = theme.value;
 
-    html.dataset.theme = toggleState;
-    localStorage.setItem('theme', toggleState);
+        html.dataset.theme = toggleState;
+        localStorage.setItem('theme', toggleState);
+    })
 });
 
 // implement theme depending on saved preferences in local storage
 function savedTheme() {
     const localStorageTheme = localStorage.getItem('theme');
-    console.log(localStorageTheme);
+
     if (localStorageTheme !== null) {
         html.dataset.theme = localStorageTheme;
-        themeSelector.value = localStorageTheme;
     }
 };
-
-// potentially put this into an on ready function at a later date
-savedTheme();
-
 
 // mobile burger menu
 const burgerIcon = document.querySelector('#burger');
@@ -30,3 +55,6 @@ const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
+
+// potentially put this into an on ready function at a later date
+savedTheme();
