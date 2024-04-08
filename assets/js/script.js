add25b456bec0a7b00514defbc0ef49ca7a7e42c
@r0f33 <jiske.nicolson@gmail.com>
2024-04-08 16:47:20 +0930

    added icons to rules page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,3 @@
-// const themeSelector = document.getElementById('theme');
-// // const themeSelector = document.querySelectorAll('option');
-// const html = document.querySelector('html');
-
-// // toggle theme
-
-// // implement theme depending on saved preferences in local storage
-// function savedTheme() {
-//     const localStorageTheme = localStorage.getItem('theme');
-//     console.log(localStorageTheme);
-//     if (localStorageTheme !== null) {
-//         html.dataset.theme = localStorageTheme;
-//         themeSelector.value = localStorageTheme;
-//     }
-// };
-
-// // potentially put this into an on ready function at a later date
-// savedTheme();
-
-// // mobile burger menu
-// const burgerIcon = document.querySelector('#burger');
-// const navbarMenu = document.querySelector('#nav-links');
-
-// burgerIcon.addEventListener('click', () => {
-//     navbarMenu.classList.toggle('is-active');
-// });
-
-// _________________________________________________________________________________________________________________
 const themeSelector = document.querySelectorAll('option');
 const html = document.querySelector('html');
 
