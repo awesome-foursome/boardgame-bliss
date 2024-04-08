e92f5e1505d75b2205d9373b4e9380494b34a4fc
@r0f33 <jiske.nicolson@gmail.com>
2024-04-08 17:38:12 +0930

    added functionality to navbar to see location

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,41 @@ burgerIcon.addEventListener('click', () => {
 
 // potentially put this into an on ready function at a later date
 savedTheme();
+
+// currentPage = document.documentElement.getAttribute('id');
+// pageLinks = document.querySelectorAll('.nabar-end a');
+// console.log(currentPage);
+
+// pageLinks.forEach((link) => {
+//     if (link.getAttribute('id') == currentPage) {
+//         document.link.style.border - border = "solid 10px";
+//     }
+
+// })
+//     ;
+
+// pages.forEach((page) => {
+//     page.addEventListener('mouseout', () => {
+
+//         const target = page.dataset.target;
+
+//         pageImages.forEach(image => {
+//             if (image.getAttribute('id') === target) {
+//                 image.classList.add('invisible');
+//                 document.querySelector('#index-img').classList.remove('invisible')
+//             }
+//         })
+//     })
+// });
+
+const currentPage = document.documentElement.getAttribute('id');
+const pageLinks = document.querySelectorAll('.navbar-end a');
+console.log(currentPage);
+
+pageLinks.forEach((link) => {
+    if (link.getAttribute('id') === currentPage) {
+        // link.style.paddingTop = "8px";
+        link.style.borderBottom = "solid 8px";
+        link.style.backgroundColor = "var(--background-color-main)";
+    }
+});
